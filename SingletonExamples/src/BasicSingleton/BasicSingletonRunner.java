53fe6b38b81016610bbc87a2019b33d6c595cda4
Jevic J <jevic.jardeleza>
2019-06-01 15:17:09 -0400

    fixed driver class to print first CD

--- SingletonExamples/src/BasicSingleton/BasicSingletonRunner.java
+++ SingletonExamples/src/BasicSingleton/BasicSingletonRunner.java
@@ -5,10 +5,10 @@ public class BasicSingletonRunner {
 	public static void main(String[] args) {
 
 		CD beatles = new CD(new String[] { "Yellow Submarine", "Hey Jude" });
-		CD drake = new CD(new String[] { "God's Plan", "Legend" });
+		CD drake = new CD(new String[] { "God's Plan", "Started From the Bottom" });
 		RecordPlayer.getRecordPlayer().setCD(beatles);
 
-		RecordPlayer.getRecordPlayer().play(1);
+		System.out.println(RecordPlayer.getRecordPlayer().play(0));
 
 		RecordPlayer.getRecordPlayer().setCD(drake);
 		
